17cd82d8c3bd6361e7079f47d03a2a0a33f0b878
Heriberto Monterrubio <heriberto.monterrubio@komodot.com>
2014-03-28 15:59:57 -0600

    First commit

---
+++ lib/src/main/java/mx/inodata/barcodereader/HelloWorld.java
@@ -0,0 +1,14 @@
+package mx.inodata.barcodereader;
+
+/**
+ * Created by heri on 28/03/14.
+ */
+public class HelloWorld {
+    public HelloWorld(){
+        System.out.println("Hello world Maven!!!");
+    }
+
+    public String getHelloText(){
+        return "Hello world Maven!!!";
+    }
+}
